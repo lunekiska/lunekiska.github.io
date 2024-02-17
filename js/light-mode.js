ac6207805e184fb030606a9b734dc77b63a8f2fe
Lunekiska - 키스카 <kiscaatwork@gmail.com>
2024-02-17 21:33:24 +0100

    Update light-mode.js

--- js/light-mode.js
+++ js/light-mode.js
@@ -4,11 +4,13 @@ button.addEventListener("click", lightModeFunction);
 function lightModeFunction() {
   var element = document.getElementById("*");
   if (button.lightMode = false) {
-  element.classList.toggle("../css/light-mode.css");
-    button.lightMode = true;
-  }
+    return element.classList.toggle("../css/light-mode.css"){
+        return button.lightMode = true;
+    };
+  };
   if (button.lightMode = true) {
-  element.classList.toggle("../css/main.css");
-    button.lightMode = false;
-  }
+    element.classList.toggle("../css/main.css"){
+      button.lightMode = false;}
+    };
+  };
 }
