39820877c3e1ec883fe73c85d390649675b10127
Lunekiska - 키스카 <kiscaatwork@gmail.com>
2024-02-17 21:16:56 +0100

    Update light-mode.js

--- js/light-mode.js
+++ js/light-mode.js
@@ -1,4 +1,7 @@
+const button = document.querySelector("input");
+button.addEventListener("click", lightModeFunction);
+
 function lightModeFunction() {
   var element = document.getElementById("*");
-  element.classList.toggle("../css/main.css");
+  element.classList.toggle("../css/light-mode.css");
 }
