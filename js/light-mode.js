85acb1923f13281b1696d750b222440ae574e5d4
Lunekiska - 키스카 <kiscaatwork@gmail.com>
2024-02-18 18:07:31 +0100

    Update light-mode.js

--- js/light-mode.js
+++ js/light-mode.js
@@ -1,5 +1,4 @@
 function changeMode(){
     document.body.classList.toggle('light');
-    document.*.classList.toggle('light');
     document.getElementById('header-menu').classList.toggle('light');
 }
